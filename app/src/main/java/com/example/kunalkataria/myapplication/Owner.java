92d55d3923d99c23463b76c878010fd3b8c23389
Kunal Kataria <kunalkataria@Kunals-MacBook-Pro-2.local>
2016-06-03 11:32:02 -0700

    made parcelable

--- app/src/main/java/com/example/kunalkataria/myapplication/Owner.java
+++ app/src/main/java/com/example/kunalkataria/myapplication/Owner.java
@@ -1,12 +1,12 @@
 package com.example.kunalkataria.myapplication;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by kunalkataria on 6/2/16.
- */
-public class Owner {
+public class Owner implements Parcelable {
 
     @SerializedName("login")
     @Expose
@@ -19,4 +19,40 @@ public class Owner {
     @SerializedName("html_url")
     @Expose
     public String htmlUrl;
+
+    public Owner (String login, String avatarUrl, String htmlUrl) {
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+        this.htmlUrl = htmlUrl;
+    }
+
+    protected Owner(Parcel in) {
+        login = in.readString();
+        avatarUrl = in.readString();
+        htmlUrl = in.readString();
+    }
+
+    public static final Creator<Owner> CREATOR = new Creator<Owner>() {
+        @Override
+        public Owner createFromParcel(Parcel in) {
+            return new Owner(in);
+        }
+
+        @Override
+        public Owner[] newArray(int size) {
+            return new Owner[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(login);
+        dest.writeString(avatarUrl);
+        dest.writeString(htmlUrl);
+    }
 }
